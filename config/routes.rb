7230cec1a06e16495333c36e98eaf2c78537a56f
Otto <ottoburney@gmail.com>
2020-02-20 19:11:59 +0000

    mister cocktail doing styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :cocktails do
+    resources :doses
+  end
+    resources :doses
+
+  # resources :doses do
+  #   resources :cocktails
+  #   resources :ingredients
+  # end
 end
